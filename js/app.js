cee1430df6bab6ffbb3dc9b5f6db924966c57356
Danelle Black <danelle.black@gmail.com>
2018-10-03 12:29:47 -0700

    got app.js linked properly.  I need to remember to add the window.onload function for working with the DOM.  My code was executing before the DOM was ready.  Live and learn

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-
-
-
-	count = 0;
+window.onload=function(){
+	let count = 0;
 	let times = document.getElementById("times");
-	let cat = document.images["cat"];
+	let cat = document.getElementById("paw");
 	let question = document.getElementById("question");
-	
-cat.onclick = function() {
-	count += 1;
-  times.innerHTML = "Times Clicked: " + count;
-  question.innerHTML = "You Clicked the Cat!";
+
+	cat.addEventListener('click', function() {
+		count += 1;
+  	times.innerHTML = "Times Clicked: " + count;
+  	question.innerHTML = "You Clicked the Cat!";
+});
+
 };
-	
